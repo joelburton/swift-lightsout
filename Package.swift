a7c6633273baf8711bdc16d58f6014f40d3f7e11
Joel Burton <joel@joelburton.com>
2023-03-22 05:53:41 -0700

    Fix configuration for newer Xcode.

--- Package.swift
+++ Package.swift
@@ -6,20 +6,18 @@ import PackageDescription
 let package = Package(
     name: "lightsout",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "lightsout",
-            targets: ["lightsout"]),
+        .executable(name: "foo", targets: ["lightsout"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "lightsout",
-            dependencies: []),
+            dependencies: [],
+            path: "."
+        ),
+        
     ]
 )
